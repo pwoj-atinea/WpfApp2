b048ff7600ef5899ab2e204765897797a9aa4614
Pawel Wojciechowski <pwoj@atinea.pl>
2022-06-07 00:20:57 +0200

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using devDept.Eyeshot.Translators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadAutodesk ra = new ReadAutodesk("1.dwg");
+            ra.DoWork();
+            ra.AddToScene(dwgView);
+        }
+    }
+}
